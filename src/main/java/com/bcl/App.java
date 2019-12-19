43a76a1480a83a2d08b2969d855f5ddedd79a1f2
dhanesh-mm <dhanesh.mm@gmail.com>
2019-12-19 22:01:58 +0530

    Initial commit

---
+++ src/main/java/com/bcl/App.java
@@ -0,0 +1,14 @@
+package com.bcl;
+
+/**
+ * BCL AUTOMATION
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "BCL Automation!" );
+    }
+}
